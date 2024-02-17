c429a08c59d7d817b3b2a963a515d6d6156b1dc9
Aditya Sharma <adi099826@gmail.com>
2024-02-18 01:10:52 +0530

    main section almost complete

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-$("html").on("click", function (e) {
+//Custom Cursor
+const ball = document.querySelector("div.cursorCustom");
+let mouseX = 0;
+let mouseY = 0;
+let ballX = 0;
+let ballY = 0;
+let speed = 0.1;
 
-    e.preventDefault();
+function animate(){
+  let distX = mouseX - ballX;
+  let distY = mouseY - ballY;
+  ballX = ballX + (distX * speed);
+  ballY = ballY + (distY * speed);
+  ball.style.left = ballX + "px";
+  ball.style.top = ballY + "px";
+  requestAnimationFrame(animate);
+}
+animate();
 
-    const href = $(this).attr("href");
+document.addEventListener("mousemove", function(event){
+  mouseX = event.pageX;
+  mouseY = event.pageY;
+})
 
-    $("html, body").animate({ scrollTop: $(href).offset().top }, 1000);
-
-});
-
+//Random circle 
 function moveDiv() {
     var $span = $(".circle");
 
