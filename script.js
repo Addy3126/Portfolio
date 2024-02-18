fc7044f879e212c72095eda49793d798a050ea14
Aditya Sharma <adi099826@gmail.com>
2024-02-18 20:58:44 +0530

    Home Section complete except photo
    may add header name animation and photo, also loading animation is pending

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 //Custom Cursor
-const ball = document.querySelector("div.cursorCustom");
-let mouseX = 0;
-let mouseY = 0;
-let ballX = 0;
-let ballY = 0;
-let speed = 0.1;
+$(document).on('mousemove', function(e) {
+  $('.cursorCustom').css({
+    left: e.clientX,
+    top: e.clientY,
+  });
+});
 
-function animate(){
-  let distX = mouseX - ballX;
-  let distY = mouseY - ballY;
-  ballX = ballX + (distX * speed);
-  ballY = ballY + (distY * speed);
-  ball.style.left = ballX + "px";
-  ball.style.top = ballY + "px";
-  requestAnimationFrame(animate);
-}
-animate();
+$(window).on('scroll', function(e) {
 
-document.addEventListener("mousemove", function(event){
-  mouseX = event.pageX;
-  mouseY = event.pageY;
+});
+
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
 })
 
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf);
+
 //Random circle 
-function moveDiv() {
-    var $span = $(".circle");
+// function moveDiv() {
+//     var $span = $(".circle");
 
-    $span.fadeOut(1000, function() {
-        var maxLeft = $(window).width() - $span.width();
-        var maxTop = $(window).height() - $span.height();
-        var leftPos = Math.floor(Math.random() * (maxLeft + 1))
-        var topPos = Math.floor(Math.random() * (maxTop + 1))
+//     $span.fadeOut(1000, function() {
+//         var maxLeft = $(window).width() - $span.width();
+//         var maxTop = $(window).height() - $span.height();
+//         var leftPos = Math.floor(Math.random() * (maxLeft + 1))
+//         var topPos = Math.floor(Math.random() * (maxTop + 1))
 
-        $span.css({ left: leftPos, top: topPos }).fadeIn(1000);
-    });
-};
+//         $span.css({ left: leftPos, top: topPos }).fadeIn(1000);
+//     });
+// };
 
-moveDiv();
-setInterval(moveDiv, 5000);
+// moveDiv();
+// setInterval(moveDiv, 5000);
