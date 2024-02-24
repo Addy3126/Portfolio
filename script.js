bb65342f59b0aa67cb92a6f460efab222566ad1d
Aditya Sharma <adi099826@gmail.com>
2024-02-24 23:50:22 +0530

    local time

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(window).on('scroll', function(e) {
 
 });
 
-
+//Smooth scroll
 const lenis = new Lenis()
 
 lenis.on('scroll', (e) => {
@@ -24,6 +24,45 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
+//Local Time
+let date = new Date();
+let options = {timeZone: 'Asia/Kolkata'};
+let localTime = date.toLocaleTimeString('en-US', options);
+let shortLocalTime = localTime.slice(0,5);
+let x = localTime.slice(9,11);
+shortLocalTime = shortLocalTime.concat(" ",x," GMT +5:30");
+document.querySelector(".header_LocalTime").innerText = shortLocalTime;
+
+function localTimeUpdate(){
+  let date = new Date();
+  let options = {timeZone: 'Asia/Kolkata'};
+  let localTime = date.toLocaleTimeString('en-US', options);
+  let shortLocalTime = localTime.slice(0,5);
+  let x = localTime.slice(9,11);
+  shortLocalTime = shortLocalTime.concat(" ",x," GMT +5:30");
+  document.querySelector(".header_LocalTime").innerText = shortLocalTime;
+  console.log(localTime)
+}
+setInterval(localTimeUpdate, 60000);
+
+// //Shuffle Effect
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// document.querySelector(".shuffleText").onmouseover = event => {
+//   let iterations = 0;
+
+//   const interval = setInterval(() => {
+//   event.target.innerText = event. target.innerText.split("").map((letter, index) => {
+//     if(index < iterations){
+//       return event.target.dataset.value[index];
+//     }
+//     return letters[Math.floor(Math.random()*26)]}).join("");
+  
+//   if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+//   iterations +=1/3;
+// }, 50);
+// }
 //Random circle 
 // function moveDiv() {
 //     var $span = $(".circle");
